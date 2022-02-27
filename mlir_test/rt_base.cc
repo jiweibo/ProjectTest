848d49667f20e7f14d638dd51a8f472e4286207d
jiweibo <jiweibo@baidu.com>
2022-02-27 15:53:34 +0000

    update

--- mlir_test/rt_base.cc
+++ mlir_test/rt_base.cc
@@ -26,10 +26,10 @@ namespace rt {
 /// Dialect initialization, the instance will be owned by the context. This is
 /// the point of registration of types and operations for the dialect.
 void RTDialect::initialize() {
-  addTypes<
-#define GET_TYPEDEF_LIST
-#include "rt_types.cpp.inc"
-      >();
+//   addTypes<
+// #define GET_TYPEDEF_LIST
+// #include "rt_types.cpp.inc"
+//       >();
 
   addTypes<ChainType>();
 
@@ -39,96 +39,96 @@ void RTDialect::initialize() {
       >();
 }
 
-// mlir::Type RTDialect::parseType(mlir::DialectAsmParser &parser) const {
+mlir::Type RTDialect::parseType(mlir::DialectAsmParser &parser) const {
+  llvm::StringRef keyword;
+  if (parser.parseKeyword(&keyword)) return nullptr;
+
+  if (keyword == "chain") return rt::ChainType::get(getContext());
+  // // parse complex type, for exampe complex<1, 1>.
+  // if (keyword == "complex") {
+  //   // parse "<"
+  //   if (parser.parseLess()) return nullptr;
+  //   int32_t first, second;
+  //   // parse first integer
+  //   if (parser.parseInteger(first)) return nullptr;
+  //   // parse ","
+  //   if (parser.parseComma()) return nullptr;
+  //   // parse second integer
+  //   if (parser.parseInteger(second)) return nullptr;
+  //   // parse ">"
+  //   if (parser.parseGreater()) return nullptr;
+
+  //   return rt::ComplexType::get(getContext(), first,
+  //   mlir::IntegerType::get(getContext(), second));
+  // }
+
+  // if (keyword == "pair") {
+  //   // parse "<"
+  //   if (parser.parseLess()) return nullptr;
+  //   int32_t first, second;
+  //   // parse first integer
+  //   if (parser.parseInteger(first)) return nullptr;
+  //   // parse ","
+  //   if (parser.parseComma()) return nullptr;
+  //   // parse second integer
+  //   if (parser.parseInteger(second)) return nullptr;
+  //   // parse ">"
+  //   if (parser.parseGreater()) return nullptr;
+
+  //   return rt::PairType::get(getContext(), first, second);
+  // }
+
+  if (auto type = mlir::Dialect::parseType(parser)) return type;
+
+  mlir::Location loc = parser.getEncodedSourceLoc(parser.getNameLoc());
+  mlir::emitError(loc) << "unknown tfrt type " << keyword;
+  return {};
+}
+
+void RTDialect::printType(mlir::Type type,
+                          mlir::DialectAsmPrinter &printer) const {
+  if (type.isa<rt::ChainType>()) {
+    printer << "chain";
+  // } else if (type.isa<rt::ComplexType>()) {
+  //   auto complexType = type.cast<rt::ComplexType>();
+  //   printer << "complex<" << complexType.getParameter() << ", " <<
+  //   complexType.getParameterType() << ">";
+  // } else if (type.isa<rt::PairType>()) {
+  //   auto pairType = type.cast<rt::PairType>();
+  //   printer << "pair<" << pairType.getFirst() << ", " << pairType.getSecond()
+  //   << ">" ;
+  } else {
+    llvm_unreachable("unknown rt type");
+  }
+}
+
+// ::mlir::Type PairType::parse(::mlir::AsmParser& parser) {
 //   llvm::StringRef keyword;
-//   if (parser.parseKeyword(&keyword)) return nullptr;
-
-//   if (keyword == "chain") return rt::ChainType::get(getContext());
-//   // parse complex type, for exampe complex<1, 1>.
-//   if (keyword == "complex") {
-//     // parse "<"
-//     if (parser.parseLess()) return nullptr;
-//     int32_t first, second;
-//     // parse first integer
-//     if (parser.parseInteger(first)) return nullptr;
-//     // parse ","
-//     if (parser.parseComma()) return nullptr;
-//     // parse second integer
-//     if (parser.parseInteger(second)) return nullptr;
-//     // parse ">"
-//     if (parser.parseGreater()) return nullptr;
-
-//     return rt::ComplexType::get(getContext(), first,
-//     mlir::IntegerType::get(getContext(), second));
-//   }
-
+//   parser.parseKeyword(&keyword);
 //   if (keyword == "pair") {
 //     // parse "<"
-//     if (parser.parseLess()) return nullptr;
+//     if (parser.parseLess())
+//       return nullptr;
 //     int32_t first, second;
 //     // parse first integer
-//     if (parser.parseInteger(first)) return nullptr;
+//     if (parser.parseInteger(first))
+//       return nullptr;
 //     // parse ","
-//     if (parser.parseComma()) return nullptr;
+//     if (parser.parseComma())
+//       return nullptr;
 //     // parse second integer
-//     if (parser.parseInteger(second)) return nullptr;
+//     if (parser.parseInteger(second))
+//       return nullptr;
 //     // parse ">"
-//     if (parser.parseGreater()) return nullptr;
+//     if (parser.parseGreater())
+//       return nullptr;
 
-//     return rt::PairType::get(getContext(), first, second);
+//     return rt::PairType::get(parser.getContext(), first, second);
 //   }
-
-//   if (auto type = mlir::Dialect::parseType(parser)) return type;
-
-//   mlir::Location loc = parser.getEncodedSourceLoc(parser.getNameLoc());
-//   mlir::emitError(loc) << "unknown tfrt type " << keyword;
-//   return {};
+//   return mlir::Type();
 // }
-
-// void RTDialect::printType(mlir::Type type,
-//                           mlir::DialectAsmPrinter &printer) const {
-//   if (type.isa<rt::ChainType>()) {
-//     printer << "chain";
-//   } else if (type.isa<rt::ComplexType>()) {
-//     auto complexType = type.cast<rt::ComplexType>();
-//     printer << "complex<" << complexType.getParameter() << ", " <<
-//     complexType.getParameterType() << ">";
-//   } else if (type.isa<rt::PairType>()) {
-//     auto pairType = type.cast<rt::PairType>();
-//     printer << "pair<" << pairType.getFirst() << ", " << pairType.getSecond()
-//     << ">" ;
-//   } else {
-//     llvm_unreachable("unknown rt type");
-//   }
+// void PairType::print(::mlir::AsmPrinter& printer) const {
+//   printer << "pair<" << getFirst() << ", " << getSecond() << ">";
 // }
-
-::mlir::Type PairType::parse(::mlir::AsmParser& parser) {
-  llvm::StringRef keyword;
-  parser.parseKeyword(&keyword);
-  if (keyword == "pair") {
-    // parse "<"
-    if (parser.parseLess())
-      return nullptr;
-    int32_t first, second;
-    // parse first integer
-    if (parser.parseInteger(first))
-      return nullptr;
-    // parse ","
-    if (parser.parseComma())
-      return nullptr;
-    // parse second integer
-    if (parser.parseInteger(second))
-      return nullptr;
-    // parse ">"
-    if (parser.parseGreater())
-      return nullptr;
-
-    return rt::PairType::get(parser.getContext(), first, second);
-  }
-  return mlir::Type();
-}
-void PairType::print(::mlir::AsmPrinter& printer) const {
-  printer << "pair<" << getFirst() << ", " << getSecond() << ">";
-}
 
 } // namespace rt
--- mlir_test/tests/test_op.mlir
+++ mlir_test/tests/test_op.mlir
@@ -3,7 +3,7 @@ func @main()-> f32 {
   %0 = "rt.constant.f32"() {value = 1.000000e+00 : f32} : () -> f32
   %1 = rt.constant.f32 2.0
   %2 = "rt.add.f32"(%0, %1) {} : (f32, f32) -> f32
-  %3 = rt.new.chain
+  %3 = "rt.new.chain"() {} : () -> !rt.chain
   "rt.print.f32"(%2, %3) {} : (f32, !rt.chain) -> !rt.chain
   rt.return %2 : f32
 }
